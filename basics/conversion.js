1ddc970baecbb14f10efcea6fb48fbd98fbd2995
marmaqi <143022038+marmaqi@users.noreply.github.com>
2023-12-19 04:28:03 +0000

    conversion

---
+++ basics/conversion.js
@@ -0,0 +1,13 @@
+let score = "Ali"
+
+
+let stringToNumber = Number(score)
+
+// console.log(typeof score);
+// console.log(typeof stringToNumber);
+// console.log(stringToNumber);
+
+// let IsLoggedIn = "1234567"
+
+// let user = Boolean(IsLoggedIn)
+// console.log(user);
