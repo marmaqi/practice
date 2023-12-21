acfe9705749b3765e98a1b8191f9c3e8c13ac730
marmaqi <143022038+marmaqi@users.noreply.github.com>
2023-12-21 07:02:15 +0000

    stack and heap

---
+++ basics/stackandheap.js
@@ -0,0 +1,18 @@
+let firstName = "ABC"
+
+let secondName = "RTY"
+
+// console.log(firstName);
+// console.log(secondName);
+
+
+let user = {
+    email: "imbalti@gmail.com",
+    phone: "023456543"
+}
+
+let secondUser = user
+secondUser.phone = "13456765432"
+
+console.log(user.email);
+console.log(secondUser.phone);
