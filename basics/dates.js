37490ce116b405691c74439ab3533e22fba330da
marmaqi <143022038+marmaqi@users.noreply.github.com>
2023-12-23 13:05:52 +0000

    Dates

---
+++ basics/dates.js
@@ -0,0 +1,8 @@
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toTimeString());
+console.log(typeof myDate);
