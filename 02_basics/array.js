607a962760b2e63342f0e0bb7e8499a40e449ec2
marmaqi <143022038+marmaqi@users.noreply.github.com>
2023-12-26 06:35:53 +0000

    edit

--- 02_basics/array.js
+++ 02_basics/array.js
@@ -1,39 +1,61 @@
-//array
+// //array
 
-const myHeros = ["IronMan", "SpiderMan"]
+// const myHeros = ["IronMan", "SpiderMan"]
 
-const myArr2 = new Array(1,2,3,4)
-// console.log(myArr[2]);
+// const myArr2 = new Array(1,2,3,4)
+// // console.log(myArr[2]);
 
-// array methods
-// myArr.push(6)
-// myArr.push(7)
-// myArr.pop()
+// // array methods
+// // myArr.push(6)
+// // myArr.push(7)
+// // myArr.pop()
 
-// myArr.unshift(0)
-// myArr.shift()
+// // myArr.unshift(0)
+// // myArr.shift()
 
-// const newArr = myArr.join();
+// // const newArr = myArr.join();
 
+// // console.log(myArr);
+// // console.log(newArr);
+// // console.log(typeof newArr);
+
+
+// // console.log(myArr.length);
+
+
+// // slice and splice
+// const myArr = [0,1,2,3,4,5]
+// // console.log("Before Slice", myArr);
+
+// // const myn1 = myArr.slice(1,3)
+
+// // console.log("Slice",myn1);
+// // console.log("After Slice",myArr);
 // console.log(myArr);
-// console.log(newArr);
-// console.log(typeof newArr);
+// const myn2 = myArr.splice(1,3)
+// // console.log("Before Splice",myArr);
+
+// console.log("Splice",myn2);
+// console.log("After Splice",myArr);
+
+const abc = ['a','b','c','d','e']
+const xyz = ['f','g','h','i','j']
+
+// const alphabet = abc.concat(xyz)
+// console.log(alphabet);
+
+// const all_new_alphabet = [...abc,...xyz]    //spread operator
+// console.log(all_new_alphabet);
+
+// console.log(Array.isArray("Muhammad"));
+// console.log(Array.from("Muhammad"));
+// console.log(Array.from({name: "Ali"}));   // interview question
+
+let number1 = 100;
+let number2 = 200;
+let number3 = 300;
+
+console.log(Array.of(number1,number2,number3));
 
 
-// console.log(myArr.length);
 
-
-// slice and splice
-const myArr = [0,1,2,3,4,5]
-// console.log("Before Slice", myArr);
-
-// const myn1 = myArr.slice(1,3)
-
-// console.log("Slice",myn1);
-// console.log("After Slice",myArr);
-console.log(myArr);
-const myn2 = myArr.splice(1,3)
-// console.log("Before Splice",myArr);
-
-console.log("Splice",myn2);
-console.log("After Splice",myArr);
