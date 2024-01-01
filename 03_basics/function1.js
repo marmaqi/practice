091e50d5439d4d90676e4ee1ff5e7662dc0438a5
marmaqi <143022038+marmaqi@users.noreply.github.com>
2024-01-01 14:01:37 +0000

    function

---
+++ 03_basics/function1.js
@@ -0,0 +1,18 @@
+function myName(){
+    console.log("Muhammad Ali");
+}
+
+// console.log(myName());
+
+function addTwoNumbers(number1, number2){
+    return number1 + number2
+}
+
+// console.log('Result = ',addTwoNumbers(8, 5))
+
+function loginMessage(username){
+    // return `${username} is just logged in` this is not run because we don't print or console it
+    return `${username} is just logged in`
+}
+
+console.log(loginMessage("Ali"))
